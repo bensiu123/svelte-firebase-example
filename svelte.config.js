be85da86610b0bbe3d15ead638a531afc55a7804
Ben Siu <ben.siu@ectest.org>
2022-07-03 20:33:26 +0800

    installed adapter, firebase init

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
+// import adapter from 'svelte-adapter-firebase';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +11,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ out: 'build' })
 	}
 };
 
